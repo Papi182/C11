b945221012ff8acf98f040be9cf1ae4b8f7484c7
Papi182 <lucaspapitendero@gmail.com>
2026-02-26 19:42:00 +0100

    a

---
+++ cromos.py
@@ -0,0 +1,10 @@
+class Cromos:
+    def __init__(self, nombre, edicion, copias, precio, estado):
+        self.nombre = nombre
+        self.edicion = edicion
+        self.marca = marca
+        self.cantidad = cantidad
+        self.precio = precio
+        self.estado = estado
+
+    def rareza(self):
