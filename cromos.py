cf6931d64b65494e3fbd92656c31e28343b0b12f
Papi182 <lucaspapitendero@gmail.com>
2026-02-27 00:52:54 +0100

    No se me ocurren que más cosas añadir de información de la carta. Mañana actualizaré el precio a algo más complejo.

--- cromos.py
+++ cromos.py
@@ -1,10 +1,35 @@
 class Cromos:
-    def __init__(self, nombre, edicion, copias, precio, estado):
+    def __init__(self, nombre, marca, edicion, copias, estado):
         self.nombre = nombre
         self.edicion = edicion
-        self.marca = marca
-        self.cantidad = cantidad
-        self.precio = precio
+        self.copias = copias
         self.estado = estado
+        self.marca = marca
 
-    def rareza(self):
+    def __str__(self):
+        return f'{self.nombre} - {self.marca} | {self.edicion} | {self.copias} | {self.estado}'
+
+    def rarezas(self):
+        rareza = ''
+        if self.copias > 10000000:
+            rareza = 'común'
+        elif self.copias > 1000000:
+            rareza = 'poco común'
+        elif self.copias > 100000:
+            rareza = 'raro'
+        elif self.copias > 10000:
+            rareza = 'super raro'
+        elif self.copias > 10:
+            rareza = 'ultra raro'
+        elif self.copias <= 10:
+            rareza = 'único'
+        return rareza
+
+    def precio(self):
+        if self.copias <= 10:
+            precio = 4**self.estado / self.copias
+        else:
+            precio = 3 ** self.estado / self.copias
+        return precio
+# Cambiar el precio y hacer que dependa también de demanda o del estado.
+
